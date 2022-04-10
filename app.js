b55de548137d30dcaed38085ff5396e3c1a59390
zermiftah <muhammadmiftahudin113@gmail.com>
2022-04-10 12:00:22 +0700

    Sending Requests and Responses

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const feedRoutes = require('./routes/feed')
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use('./feed', feedRoutes)
+//app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
--- routes/feed.js
+++ routes/feed.js
@@ -5,5 +5,6 @@ const feedController = require('../controllers/feed')
 const router = express.Router();
 
 router.get('/posts', feedController.getPost);
+router.post('/post', feedController.createPost);
 
 module.exports = router;
